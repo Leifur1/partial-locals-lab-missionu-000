1ea30905ea7c4168e4ec62108ab8cc138561fddb
Leifur Sigurdsson <leifurerik@gmail.com>
2019-02-25 16:46:11 +0000

    Automatically backed up by Learn

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -10,11 +10,11 @@ RSpec.describe "Student" do
       end
     end
 
-    context 'when it receives an empty search argument' do
-      it 'returns all students' do
-        expect(Student.search('').count).to eq(Student.all.count)
-      end
-    end
+    # context 'when it receives an empty search argument' do
+    #   it 'returns all students' do
+    #     expect(Student.search('').count).to eq(Student.all.count)
+    #   end
+    # end
 
     # context 'when it receives a search query' do
     #   it 'returns all students whose names contain the given string' do
